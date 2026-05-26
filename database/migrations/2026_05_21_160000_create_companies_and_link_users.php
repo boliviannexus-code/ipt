01ac3300cb46cb802a29ca369d5ff4ece77c5435
Pachekurt <boliviannexus@gmail.com>
2026-05-26 18:25:59 -0400

    basepostgre

--- database/migrations/2026_05_21_160000_create_companies_and_link_users.php
+++ database/migrations/2026_05_21_160000_create_companies_and_link_users.php
@@ -27,7 +27,6 @@ return new class extends Migration
         Schema::table('users', function (Blueprint $table): void {
             $table->foreignId('company_id')
                 ->nullable()
-                ->after('id')
                 ->constrained('companies')
                 ->nullOnDelete();
 
